a877b92de6c848444087b5f4d81ab760fb0e69b4
Aryan7Sharma <aryansharma2003march@gmail.com>
2022-12-11 16:18:38 +0530

    c

---
+++ Learning Part1/Hello.js
@@ -0,0 +1,5 @@
+console.log("hello world");
+console.log('single quote');
+console.log(`backties`);
+// for comments console.log
+console.log('console');
---
+++ Learning Part1/part1.js
@@ -0,0 +1,5 @@
+// all about variables
+// "use strict"
+// console.log(x);
+let x = "aryan sharma";
+console.log(x.slice(-3,-1));
